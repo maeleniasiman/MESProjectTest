41c9d57e643f4f3e0444944fbe6f2c54194dafcb
Elaine <e.siman@cambria.com>
2023-11-12 06:40:28 +0800

    updated model

---
+++ Controllers/ContactFormController.cs
@@ -0,0 +1,33 @@
+﻿using MESProject.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace MESProject.Controllers
+{
+	public class ContactFormController : SurfaceController
+	{
+		public ContactFormController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+		{
+		}
+
+        [HttpPost]
+		[ValidateAntiForgeryToken]
+        public ActionResult HandleContactForm(ContactFormViewModel model)
+		{
+			if (!ModelState.IsValid)
+			{
+				ModelState.AddModelError("Error", "Please check the form.");
+				return PartialView("~/Views/Partials/Contact Form.cshtml", model);
+			}
+
+			return null;
+		}
+	}
+}
---
+++ ViewModels/ContactFormViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MESProject.ViewModels
+{
+	public class ContactFormViewModel
+	{
+		[Required]
+		public string Name { get; set; }
+
+		[Required]
+		[EmailAddress(ErrorMessage = "Please enter a valid email address")]
+		public string Email { get; set; }
+
+		[Required]
+		public string Comment { get; set; }
+
+		[Required]
+		public string Subject { get; set; }
+	}
+}
